44c4869af5979fef4ab195d2459a9cf1fc664976
mmatveyev <progl@progl.su>
2023-03-12 11:45:48 +0300

    excluding blocks

--- bot/configuration.py
+++ bot/configuration.py
@@ -249,7 +249,8 @@ class CameraConfig(ConfigHelper):
         self.light_timeout: int = self._get_int("light_control_timeout", default=0, min_value=0)
         self.picture_quality: str = self._get_str("picture_quality", default="high", allowed_values=["low", "high"])
         self.cv2_params = config.items("camera.cv2") if config.has_section("camera.cv2") else []
-        self.name = config.items(f"{self._section}.name") if config.has_section(f"{self._section}.name") else None
+        self.name = self._get_str('name', default=None)
+
 
 class CameraConfig2(CameraConfig):
     _section = "camera2"
